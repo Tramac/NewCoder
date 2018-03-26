1e64543f4c67536983463ea27f86045cd6db9df6
Tramac <1145955920@qq.com>
2018-03-26 19:12:20 +0800

    Update MagicCoin.cpp

--- WangYi2018/MagicCoin.cpp
+++ WangYi2018/MagicCoin.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
+#include <stack>
 
+using namespace std;
+void MagicCoin(int n)
+{
+	if (n <= 0)
+		return;
+	stack<int> s;
+	while (n > 0)
+	{
+		if (n % 2 == 0)
+		{
+			s.push(2);
+			n = (n - 2) / 2;
+		}
+		if (n % 2 == 1)
+		{
+			s.push(1);
+			n = (n - 1) / 2;
+		}
+	}
+	while (!s.empty())
+	{
+		cout << s.top();
+		s.pop();
+	}
+	cout << endl;
+	return;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	MagicCoin(n);
+	return 0;
+}
